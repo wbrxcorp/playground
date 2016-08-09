f599dce7d0c691fa10f7cdd5b104accfbe0978e0
Tomoatsu Shimada <shimada@walbrix.com>
2016-08-09 19:05:53 +0900

    ioutils fix

--- jee/src/main/scala/modules/ioutils/package.scala
+++ jee/src/main/scala/modules/ioutils/package.scala
@@ -8,21 +8,19 @@ import org.apache.commons.io.LineIterator
 // https://commons.apache.org/proper/commons-io/javadocs/api-release/index.html?org/apache/commons/io/package-summary.html
 package object ioutils {
   val defaultCharsetName = "UTF-8"
-  val defaultCharset = Charset.forName(defaultCharsetName)
+  implicit def string2charset(charsetName:String):Charset = Charset.forName(charsetName)
+  val defaultCharset = string2charset(defaultCharsetName)
 
   def contentEquals(input1:InputStream, input2:InputStream):Boolean = IOUtils.contentEquals(input1, input2)
   def contentEquals(input1:Reader, input2:Reader):Boolean = IOUtils.contentEquals(input1, input2)
   def contentEqualsIgnoreEOL(input1:Reader, input2:Reader):Boolean = IOUtils.contentEqualsIgnoreEOL(input1, input2)
   def copy(input:InputStream, output:OutputStream):Int = IOUtils.copy(input, output)
   def copy(input:InputStream, output:OutputStream, bufferSize:Int):Long = IOUtils.copy(input, output, bufferSize)
-  def copy(input:InputStream, output:Writer, inputEncoding:Charset=defaultCharset):Unit = IOUtils.copy(input, output, inputEncoding)
-  def copy(input:InputStream, output:Writer, inputEncoding:String):Unit = IOUtils.copy(input, output, inputEncoding)
-  def copy(input:Reader, output:OutputStream, outputEncoding:Charset=defaultCharset):Unit = IOUtils.copy(input, output, outputEncoding)
-  def copy(input:Reader, output:OutputStream, outputEncoding:String):Unit = IOUtils.copy(input, output, outputEncoding)
+  def copy(input:InputStream, output:Writer, inputEncoding:Charset):Unit = IOUtils.copy(input, output, inputEncoding)
+  def copy(input:Reader, output:OutputStream, outputEncoding:Charset):Unit = IOUtils.copy(input, output, outputEncoding)
   def copy(input:Reader, output:Writer):Int = IOUtils.copy(input, output)
   // :
   def lineIterator(input:InputStream, encoding:Charset=defaultCharset):LineIterator = IOUtils.lineIterator(input, encoding)
-  def lineIterator(input:InputStream, encoding:String):LineIterator = IOUtils.lineIterator(input, encoding)
   def lineIterator(input:Reader):LineIterator = IOUtils.lineIterator(input)
   def read(input:InputStream, buffer:Array[Byte]):Int = IOUtils.read(input, buffer)
   def read(input:InputStream, buffer:Array[Byte], offset:Int, length:Int):Int = IOUtils.read(input, buffer, offset, length)
@@ -30,15 +28,12 @@ package object ioutils {
   def toByteArray(input:InputStream):Array[Byte] = IOUtils.toByteArray(input)
   // :
   def toByteArray(input:Reader, encoding:Charset=defaultCharset):Array[Byte] = IOUtils.toByteArray(input)
-  def toByteArray(input:Reader, encoding:String):Array[Byte] = IOUtils.toByteArray(input)
   // :
   def toString(input:InputStream, encoding:Charset=defaultCharset):String = IOUtils.toString(input, encoding)
-  def toString(input:InputStream, encoding:String):String = IOUtils.toString(input, encoding)
   def toString(input:Reader):String = IOUtils.toString(input)
   // :
   def write(data:Array[Byte], output:Writer, encoding:Charset=defaultCharset):Unit = IOUtils.write(data, output, encoding)
   def write(data:Array[Byte], output:Writer, encoding:String):Unit = IOUtils.write(data, output, encoding)
   // :
-  def write(data:String, output:OutputStream, encoding:Charset=defaultCharset):Unit = IOUtils.write(data, output, encoding)
   def write(data:String, output:OutputStream, encoding:String):Unit = IOUtils.write(data, output, encoding)
 }
