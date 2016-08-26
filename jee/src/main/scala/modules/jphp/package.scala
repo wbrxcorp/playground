e65cac8bdafacefbcd463ef9c6809171221bd3ea
Tomoatsu Shimada <shimada@walbrix.com>
2016-08-26 10:19:17 +0900

    more investigation about jphp

--- jee/src/main/scala/modules/jphp/package.scala
+++ jee/src/main/scala/modules/jphp/package.scala
@@ -1,8 +1,39 @@
 package modules
 
+import collection.JavaConversions._
 import php.runtime.env.{CompileScope,ConcurrentEnvironment,Context,CallStackItem,TraceInfo}
+import php.runtime.memory.{LongMemory,ArrayMemory}
+import php.runtime.reflection.ModuleEntity
 
 package object jphp {
+  val random = new scala.util.Random
+
+  def compilephp(inputStream:java.io.InputStream,file:String):ModuleEntity = {
+    val compileScope = new CompileScope
+    val environment = new ConcurrentEnvironment(compileScope, System.out)
+    val context = new Context(inputStream, file, environment.getDefaultCharset)
+    val compiler = new org.develnext.jphp.core.compiler.jvm.JvmCompiler(environment, context);
+    compiler.compile(false);
+  }
+
+  def execphp(moduleEntity:ModuleEntity):Unit = {
+    val compileScope = new CompileScope
+    val environment = new ConcurrentEnvironment(compileScope, System.out)
+    compileScope.loadModule(moduleEntity)
+    compileScope.addUserModule(moduleEntity)
+    environment.registerModule(moduleEntity)
+    environment.getGlobals.put("_SESSION", ArrayMemory.ofMap(Map("userId"->LongMemory.valueOf(random.nextInt(100)))))
+    environment.pushCall(new CallStackItem(new TraceInfo(moduleEntity.getName, -1, -1)));
+    try {
+      moduleEntity.includeNoThrow(environment)
+    }
+    finally {
+      environment.popCall
+      compileScope.triggerProgramShutdown(environment)
+      environment.doFinal
+    }
+  }
+
   def execphp(inputStream:java.io.InputStream,file:String):Unit = {
     val compileScope = new CompileScope
     val environment = new ConcurrentEnvironment(compileScope, System.out)
@@ -13,6 +44,7 @@ package object jphp {
     compileScope.loadModule(moduleEntity)
     compileScope.addUserModule(moduleEntity)
     environment.registerModule(moduleEntity)
+    environment.getGlobals.put("_SESSION", ArrayMemory.ofMap(Map("userId"->LongMemory.valueOf(2))))
 
     environment.pushCall(new CallStackItem(new TraceInfo(moduleEntity.getName, -1, -1)));
     try {
@@ -27,12 +59,17 @@ package object jphp {
 
   // modules.jphp.execphpfile("hoge.php")
   def execphpfile(file:String):Unit = {
-    val inputStream = new java.io.FileInputStream(file)
-    execphp(inputStream, file)
+    modules.common.using(new java.io.FileInputStream(file)) { inputStream =>
+      execphp(inputStream, file)
+    }
   }
 
   // modules.jphp.execphp("<?php echo 'hoge'?>")
   def execphp(script:String):Unit = {
     execphp(new java.io.ByteArrayInputStream(script.getBytes("UTF-8")), "script")
+  }
+
+  def compilephp(script:String):ModuleEntity = {
+    compilephp(new java.io.ByteArrayInputStream(script.getBytes("UTF-8")), "script")
   }
 }
